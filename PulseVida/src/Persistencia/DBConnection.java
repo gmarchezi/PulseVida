e780547d441d17e008f10142e216ea64ae3cd950
hmrebello <hmrebello.higor@gmail.com>
2018-10-23 11:07:02 -0300

    Criação da classe java pra conexão com BD

---
+++ PulseVida/src/Persistencia/DBConnection.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+/**
+ *
+ * @author hmreb
+ */
+public class DBConnection {
+    private static Connection instance = null;
+    
+    private DBConnection() {
+    }
+    
+    public static synchronized Connection getInstance() {
+        if (instance == null) {
+            try {
+                Connection c = null;
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:pulseVida.db");
+                c.setAutoCommit(true);
+                instance = c;
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            }
+        }
+        return instance;
+    }
+}
