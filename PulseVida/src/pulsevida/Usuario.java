82a0e3675262fa54a808bb7360c93b69c1cc7ae3
hmrebello <hmrebello.higor@gmail.com>
2018-11-05 09:25:41 -0200

    Atualização da Classe Usuário e adição da Classe InsertUsuario na persistência

--- PulseVida/src/pulsevida/Usuario.java
+++ PulseVida/src/pulsevida/Usuario.java
@@ -20,4 +20,22 @@ public class Usuario extends Pessoa {
         this.email = email;
         this.senha = senha;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
 }
