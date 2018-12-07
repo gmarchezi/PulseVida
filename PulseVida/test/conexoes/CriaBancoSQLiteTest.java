101416809cca29ca2293a4f270d7aaacf6fb8fc1
Guilherme-Bodart <guilhermebodart73@gmail.com>
2018-12-06 22:51:02 -0200

    Novo
    nOVO

---
+++ PulseVida/test/conexoes/CriaBancoSQLiteTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexoes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class CriaBancoSQLiteTest {
+    
+    public CriaBancoSQLiteTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
--- PulseVida/test/persistencia/UsuarioDAOTest.java
+++ PulseVida/test/persistencia/UsuarioDAOTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package conexoes;
+package persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
