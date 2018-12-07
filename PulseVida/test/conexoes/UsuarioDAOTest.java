db3130a70251c5b48edbc7a49e582e9d708ac74f
Guilherme-Bodart <guilhermebodart73@gmail.com>
2018-12-06 22:13:14 -0200

    Testes
    Teste UsuarioDAO

--- PulseVida/test/conexoes/UsuarioDAOTest.java
+++ PulseVida/test/conexoes/UsuarioDAOTest.java
@@ -5,10 +5,44 @@
  */
 package conexoes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import persistencia.UsuarioDAO;
+import pulsevida.Usuario;
+
 /**
  *
  * @author Guilherme
  */
 public class UsuarioDAOTest {
+    
+    public UsuarioDAOTest() {
+    }
+
+    @Test
+    public void Salvar() throws ClassNotFoundException, SQLException {
+        Usuario usr = new Usuario(999,"Testador","27123456789","testador@hotmail.com","testador34","testando123");
+        UsuarioDAO usrDAO = new UsuarioDAO();
+        usrDAO.salvar(usr);
+        
+        Connection c = null;
+        Statement stmt = null;       
+        
+        Class.forName("org.sqlite.JDBC");
+        c = DriverManager.getConnection("jdbc:sqlite:pulseVidaDB.db");  
+        stmt = c.createStatement();
+         
+        ResultSet rs = stmt.executeQuery("SELECT * FROM tbl_usuario WHERE ID=999");
+        
+        assertEquals(rs.getInt("ID"), usr.getId());
+        assertEquals(rs.getString("NOME"), usr.getNome());
+        assertEquals(rs.getString("EMAIL"), usr.getEmail());
+        assertEquals(rs.getString("LOGIN"), usr.getLogin());
+    }
     
 }
